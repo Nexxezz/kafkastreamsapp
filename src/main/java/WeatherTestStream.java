d90f8c001e1e320bee6533526806b920d5d29e55
Alelsandr <samsonovv.work@gmail.com>
2019-12-23 10:04:27 +0300

    fixed serde for weather message

--- src/main/java/WeatherTestStream.java
+++ src/main/java/WeatherTestStream.java
@@ -1,32 +1,44 @@
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.*;
 
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.CountDownLatch;
 
-public class Pipe {
+
+public class WeatherTestStream {
+    static public class WeatherMessage {
+        public Long lng;
+        public Long lat;
+        public Long avgTmprF;
+        public Long avgTmprC;
+        public String wthDate;
+    }
+
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "stream-weather-test");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "sandbox-hdp.hortonworks.com:6667");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         StreamsBuilder builder = new StreamsBuilder();
-        builder.stream("hotels-topic-test").map((key, value) -> KeyValue.pair(value.toString().split(",")[7], value)).to("hotels-topic-output");
+
+        /**
+         * Try to run jar with this line, then change target topic and repeat
+         */
+        builder.stream("weather-topic").to("weather-topic-output");
         final Topology topology = builder.build();
         final KafkaStreams streams = new KafkaStreams(topology, props);
+        System.out.println(topology.describe());
         final CountDownLatch latch = new CountDownLatch(1);
 
-        Runtime.getRuntime().
-
-                addShutdownHook(new Thread("streams-shutdown-hook") {
-                    @Override
-                    public void run() {
-                        streams.close();
-                        latch.countDown();
-                    }
-                });
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook") {
+            @Override
+            public void run() {
+                streams.close();
+                latch.countDown();
+            }
+        });
 
         try {
             streams.start();
