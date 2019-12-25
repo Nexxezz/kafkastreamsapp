e928bbfd64f2a474f6c6290bbf0f5d92caa211dc
Evgenii Rybnikov <evgenii_rybnikov@epam.com>
2019-12-25 20:41:32 +0300

    junit + hotel serde test

--- src/main/java/kafka/data/Hotel.java
+++ src/main/java/kafka/data/Hotel.java
@@ -1,30 +1,36 @@
 package kafka.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class Hotel {
+
     @JsonProperty("id")
     private int id;
+
     @JsonProperty("name")
     private String name;
+
     @JsonProperty("country")
     private String country;
+
     @JsonProperty("city")
     private String city;
+
     @JsonProperty("address")
     private String address;
+
     @JsonProperty("latitude")
     private String latitude;
+
     @JsonProperty("longitude")
     private String longitude;
+
     @JsonProperty("geoHash")
     private String geoHash;
 }
