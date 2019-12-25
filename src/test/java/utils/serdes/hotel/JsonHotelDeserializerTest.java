679bce4aad1156e38e960a11829b0db54f043b4b
Evgenii Rybnikov <evgenii_rybnikov@epam.com>
2019-12-25 21:52:58 +0300

    deserializator test revert

--- src/test/java/utils/serdes/hotel/JsonHotelDeserializerTest.java
+++ src/test/java/utils/serdes/hotel/JsonHotelDeserializerTest.java
@@ -11,9 +11,13 @@ class JsonHotelDeserializerTest {
     @Test
     void deserialize() {
         JsonHotelDeserializer deserializer = new JsonHotelDeserializer();
-        String[] message = "3427383902209,H tel Barri re Le Fouquet s,FR,Paris,46 Avenue George V 8th arr 75008 Paris France,48.8710709,2.3013119,u09wh".split(",");
-        Hotel actualHotel = deserializer.deserialize("topic","3427383902209,H tel Barri re Le Fouquet s,FR,Paris,46 Avenue George V 8th arr 75008 Paris France,48.8710709,2.3013119,u09wh".getBytes());
-        Hotel expectedHotel = new Hotel(Long.parseLong(message[0]),message[1],message[2],message[3],message[4],message[5],message[6],message[7]);
-        assertEquals(expectedHotel,actualHotel);
+
+        byte[] bytes = "4,Best Western Holiday Hills,US,Coalville,500 W 120 S,40.91089,-111.40339,9x2b8".getBytes();
+        Hotel actualHotel = deserializer.deserialize("topic", bytes);
+
+        Hotel expectedHotel = new Hotel(4L, "Best Western Holiday Hills", "US",
+                "Coalville", "500 W 120 S", "40.91089", "-111.40339", "9x2b8");
+
+        assertEquals(expectedHotel, actualHotel);
     }
 }
