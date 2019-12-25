250267168a3359feb9d4e2075878fc7debcd67d7
Evgenii Rybnikov <evgenii_rybnikov@epam.com>
2019-12-25 22:24:56 +0300

    things

--- src/main/java/kafka/app/EvgeniiKafkaStreamExp.java
+++ src/main/java/kafka/app/EvgeniiKafkaStreamExp.java
@@ -1,7 +1,10 @@
 package kafka.app;
 
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,28 +15,34 @@ public class EvgeniiKafkaStreamExp {
     public static final Logger LOG = LoggerFactory.getLogger(EvgeniiKafkaStreamExp.class);
 
     public static void main(String[] args) {
-        String topic = args[1];
-        LOG.info("START with arg={}", topic);
+        LOG.info("START");
 
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        final KStream<String, String> weather = builder.stream("weather-topic");
+//        weather.map()
+        weather.to("SOME_OTHER_TOPIC");
+
+
+        proceedStreams(builder);
+
+        LOG.info("END");
+    }
+
+    private static void proceedStreams(StreamsBuilder builder) {
         Properties props = getProperties();
-
-
+        final KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.cleanUp();
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
     private static Properties getProperties() {
         Properties streamsConfiguration = new Properties();
-        // Give the Streams application a unique name.  The name must be unique in the Kafka cluster
-        // against which the application is run.
         streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "evgenii-kafka-stream-exp");
-        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "evgenii-kafka-stream-exp-client");
-
-        // Where to find Kafka broker(s).
         streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "sandbox-hdp.hortonworks.com:6667");
-
-        // Specify default (de)serializers for record keys and for record values.
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass().getName());
-        //streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.ByteArray().getClass().getName());
-        return null;
+        return streamsConfiguration;
     }
 }
