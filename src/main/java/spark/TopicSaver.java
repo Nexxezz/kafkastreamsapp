4259589b281a3d77479ae992be03a8f5e1a44ee6
Alelsandr <samsonovv.work@gmail.com>
2020-01-09 21:41:03 +0300

    fixed imports

--- src/main/java/spark/TopicSaver.java
+++ src/main/java/spark/TopicSaver.java
@@ -1,14 +1,20 @@
 package spark;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 public class TopicSaver {
     public static String TOPIC_NAME = "join-result-topic";
-    public static String HDFS_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp/dataset";
+    public static String HDFS_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp";
     public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder().appName("Application").getOrCreate();
         Dataset<Row> df = spark
                 .readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
                 .option("subscribe", TOPIC_NAME)
                 .load();
-        df.write.parquet(HDFS_PATH);
+        df.write().parquet(HDFS_PATH);
     }
 }
