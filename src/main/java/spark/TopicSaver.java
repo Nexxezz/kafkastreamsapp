e3abfb68fcead42c609777cb630383afdcd56533
Alelsandr <samsonovv.work@gmail.com>
2020-01-09 21:01:43 +0300

    added constants

--- src/main/java/spark/TopicSaver.java
+++ src/main/java/spark/TopicSaver.java
@@ -1,11 +1,14 @@
 package spark;
 public class TopicSaver {
+    public static String TOPIC_NAME = "join-result-topic";
+    public static String HDFS_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp/dataset";
     public static void main(String[] args) {
         Dataset<Row> df = spark
                 .readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
-                .option("subscribe", "join-result-topic")
+                .option("subscribe", TOPIC_NAME)
                 .load();
+        df.write.parquet(HDFS_PATH);
     }
 }
