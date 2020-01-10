19498590042e1145e3ff2bb071145d0d7945d799
Alelsandr <samsonovv.work@gmail.com>
2020-01-10 20:44:13 +0300

    fixed spark Session options

--- src/main/java/spark/TopicSaver.java
+++ src/main/java/spark/TopicSaver.java
@@ -5,18 +5,23 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class TopicSaver {
-    public static String TOPIC_NAME = "join-result-topic";
-    public static String HDFS_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp";
+    public static String TOPIC_NAME = "hotels`-topic";
+    public static String HDFS_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp/test";
+    public static String CHECKPOINT_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp/spark_checkpoints";
 
     public static void main(String[] args) {
-        SparkSession spark = SparkSession.builder().appName("Application").getOrCreate();
+        SparkSession spark = SparkSession.builder()
+                .master("local[*]")
+                .appName("Application").getOrCreate();
+
         Dataset<Row> df = spark
                 .readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
                 .option("subscribe", TOPIC_NAME)
                 .load();
+
         df.printSchema();
-        df.write().parquet(HDFS_PATH);
+        df.writeStream().format("parquet").option("checkpointLocation",CHECKPOINT_PATH).start(HDFS_PATH);
     }
 }
