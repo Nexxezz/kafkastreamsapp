e1dd78f588fc402d508c03a74cf90a39295b0b32
Evgenii Rybnikov <evgenii_rybnikov@epam.com>
2020-01-15 23:08:17 +0300

    buffer added

--- src/main/java/spark/TopicSaver3.java
+++ src/main/java/spark/TopicSaver3.java
@@ -26,74 +26,84 @@ public class TopicSaver3 {
 
     public static final String TOPIC_NAME = "join-result-topic";
     public static final String HDFS_PATH = "hdfs://sandbox-hdp.hortonworks.com:8020/tmp/topicSaverResult";
-    public static final int RECORDS_LIMIT = 1000;
+
+    private static final int KAFKA_MAX_POLL_RECORDS = 1000;
+    private static final int KAFKA_POLL_TIMEOUT_MS = 10000;
+    private static final int RECORDS_BUFFER_SIZE = 100_000;
 
     public static void main(String[] args) {
         String topic;
         String path;
-        int limit;
 
         if (args.length == 0) {
             topic = TOPIC_NAME;
             path = HDFS_PATH;
-            limit = RECORDS_LIMIT;
         } else {
             topic = args[0];
             path = args[1];
-            limit = Integer.parseInt(args[2]);
         }
 
         final String groupId = UUID.randomUUID().toString().substring(0, 5);
 
-        LOG.info("Starting TopicSaver3 with parameters: topic={}, path={}, limit={}, groupId={}",
-                topic, path, limit, groupId);
+        LOG.info("Starting TopicSaver3 with parameters: topic={}, path={}, groupId={}",
+                topic, path, groupId);
 
+        try (SparkSession ss = initSpark();
+             KafkaConsumer<byte[], HotelWeather> consumer = getConsumer(getProps(groupId), topic)) {
 
-        final KafkaConsumer<byte[], HotelWeather> consumer = getConsumer(getProps(groupId), topic);
-        SparkSession ss = initSpark();
-
-        try {
+            ArrayList<HotelWeather> buffer = new ArrayList<>(RECORDS_BUFFER_SIZE);
             int totalRead = 0;
             boolean isTopicEmpty = false;
 
             while (!isTopicEmpty) {
-                final ConsumerRecords<byte[], HotelWeather> consumerRecords = consumer.poll(10000);
-
+                List<HotelWeather> records = Collections.emptyList();
+                final ConsumerRecords<byte[], HotelWeather> consumerRecords = consumer.poll(KAFKA_POLL_TIMEOUT_MS);
                 if (consumerRecords.count() == 0) {
-                    LOG.debug("no more messages in the topic, messages read total={}", totalRead);
+                    LOG.trace("no more messages in the topic, messages read total={}", totalRead);
                     isTopicEmpty = true;
                 } else {
+                    LOG.trace("read {} records from topic", consumerRecords.count());
 //                    if (LOG.isDebugEnabled()) {
 //                        consumerRecords.forEach(record -> LOG.debug("key={}, value={}", record.key(), record.value()));
 //                    }
-                    List<HotelWeather> filteredRecords = createStream(consumerRecords.iterator())
+                    records = createStream(consumerRecords.iterator())
                             .map(ConsumerRecord::value)
                             .filter(Objects::nonNull)
-                            .filter(HotelWeather::nonNull)
                             .collect(Collectors.toList());
-
-                    saveToHdfsBySpark(filteredRecords, ss, path);
-
                     totalRead += consumerRecords.count();
                     consumer.commitAsync();
                 }
+                saveToHdfs(buffer, records, isTopicEmpty, ss, path);
             }
         } finally {
             LOG.debug("final section of try");
-            consumer.close();
-            ss.stop();
         }
-
         LOG.info("END");
+    }
+
+    private static void saveToHdfs(List<HotelWeather> buffer, List<HotelWeather> records,
+                                   boolean isTopicEmpty, SparkSession ss, String path) {
+        if (isTopicEmpty) {
+            LOG.trace("saving rest of the buffer to hdfs, size={}", buffer.size());
+            saveToHdfsBySpark(buffer, ss, path);
+        } else if (buffer.size() + records.size() >= RECORDS_BUFFER_SIZE) {
+            LOG.trace("no free space in buffer, saving its records to hdfs");
+            saveToHdfsBySpark(buffer, ss, path);
+            buffer.clear();
+            buffer.addAll(records);
+        } else {
+            buffer.addAll(records);
+            LOG.trace("buffer is filled in with new records, size now is {}", buffer.size());
+        }
     }
 
     private static void saveToHdfsBySpark(List<HotelWeather> records,
                                           SparkSession ss,
                                           String path) {
         JavaSparkContext sparkContext = new JavaSparkContext(ss.sparkContext());
-
         JavaRDD<HotelWeather> rdd = sparkContext.parallelize(records);
         Dataset<Row> df = ss.createDataFrame(rdd, Weather.class);
+        LOG.trace("appending output with {} records", records.size());
         df.coalesce(1).write().mode(SaveMode.Append).parquet(path);
     }
 
@@ -120,18 +130,17 @@ public class TopicSaver3 {
         Properties props = new Properties();
         props.put("group.id", groupId);
         props.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667");
-        props.put("max.poll.records", 1000);
+        props.put("max.poll.records", KAFKA_MAX_POLL_RECORDS);
         props.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
         props.put("value.deserializer", "kafka.serdes.HotelWeatherDeserializer");
-
-        //not a good idea, see
-        //https://stackoverflow.com/questions/28561147/how-to-read-data-using-kafka-consumer-api-from-beginning
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "topic-saver3-id");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
 //        props.put("enable.auto.commit", "true");
 //        props.put("auto.commit.interval.ms", "1000");
 //        props.put("session.timeout.ms", "30000");
+
+        //probably not a good idea, see
+        //https://stackoverflow.com/questions/28561147/how-to-read-data-using-kafka-consumer-api-from-beginning
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "topic-saver3-id");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return props;
     }
 
